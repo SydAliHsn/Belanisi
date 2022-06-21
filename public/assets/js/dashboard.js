52c4e0eca3702466543797b8754343159c107948
SMAliHassan <syedalihassan6651@GMAIL.COM>
2022-06-21 22:31:07 +0500

    Minor Changes

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -7,3 +7,24 @@ sidebarBtn.addEventListener("click", () => {
     sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
   } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
 });
+
+const getOrders = async () => {
+  const res = await fetch("/api/v1/orders");
+  const { data } = await res.json();
+
+  return data.orders;
+};
+
+const getUsers = async () => {
+  const res = await fetch("/api/v1/users");
+  const { data } = await res.json();
+
+  return data.users;
+};
+
+const getProducts = async () => {
+  const res = await fetch("/api/v1/users");
+  const { data } = await res.json();
+
+  return data.users;
+};
